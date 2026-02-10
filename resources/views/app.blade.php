847a46fda3f7600116b3aba2e613a8b17e619bf5
hasnatech <mcader@hasnatech.com>
2026-02-10 21:44:05 +0530

    published

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,14 +13,14 @@
     <link rel="icon" href="/images/icon.png" type="image/png">
     {{--
     <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
-    <link rel="apple-touch-icon" href="/icon.png">
+    <link rel="apple-touch-icon" href="/images/icon.png">
 
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <link rel="manifest" href="/build/manifest.webmanifest">
-    <meta name="theme-color" content="#644f29">
+    <meta name="theme-color" content="#07461fff">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Dua App">
--- vite.config.ts
+++ vite.config.ts
@@ -28,13 +28,13 @@ export default defineConfig({
                 theme_color: '#02270cff',
                 icons: [
                     {
-                        src: '/dark.png',
+                        src: '/images/icon.png',
                         sizes: '192x192',
                         type: 'image/png',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/dark.png',
+                        src: '/images/icon.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable'
